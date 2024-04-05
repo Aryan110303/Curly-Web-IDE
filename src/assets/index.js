a4e78b1a41c3825222ba68ba7dcf4acba633ca74
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-05 11:53:03 +0530

    added moon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,6 @@
 import codeplanet from "./codeplanet.png";
 import grid from "./grid.png";
 import profile from "./profile.png";
+import moon from "./moon.mp4";
 
-export { codeplanet, grid, profile };
+export { codeplanet, grid, profile, moon };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,43 @@
-import { profile } from "../assets";
+import { moon } from "../assets";
 import Button from "./Button";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <Button className=" " href="#login">
-        sTART NOW!
+    <div className="hero mt-10">
+      <video
+        height={80}
+        width={800}
+        className="hero-moon absolute left-[15.5rem] top-[9rem] z-10"
+        muted
+        autoPlay
+        data-aos="zoom-in"
+        data-aos-delay="1000"
+        data-aos-anchor-placement="top top"
+      >
+        <source src={moon} type="video/mp4" />
+      </video>
+      <div className="hero-content">
+        <h1
+          className="font-thunder text-8xl uppercase tracking-wide mt-6 ml-15"
+          data-aos="fade-left"
+          data-aos-delay="300"
+        >
+          A Planeterium Space
+        </h1>
+        <h1
+          className="font-thunder text-8xl uppercase tracking-wide text-right mr-15 mt-[17rem]"
+          data-aos="fade-right"
+          data-aos-delay="500"
+          data-aos-anchor-placement="top top"
+        >
+          for developers
+        </h1>
+      </div>
+      <Button className=" " href="">
+        START NOW!
       </Button>
     </div>
   );
