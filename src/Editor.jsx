a58753f34b7dad6ed14db1ac3b69421ca0df40d6
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-13 14:01:52 +0530

    test

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import LoginButton from "./components/Login";
-import Fail from "./components/Fail";
-import IDE from "./components/IDE";
 
 
 const Editor = () => {
