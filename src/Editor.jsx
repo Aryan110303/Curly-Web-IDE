d5a603554280fbbb1f09be5da125a44b1dc3e023
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-13 14:38:22 +0530

    check

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,21 +1,22 @@
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/Login";
+import EditorBtn from "./components/EditorBtn";
+
 
 const Editor = () => {
-    const { isAuthenticated } = useAuth0();
+    const { user, isAuthenticated, isLoading } = useAuth0();
     const { loginWithRedirect } = useAuth0();
   return (
-    <>
     <div>
     {isAuthenticated ? (
-          <h1>
-          Logout
-          </h1>
+          <EditorBtn  linkTo="/">
+              Back
+          </EditorBtn>
       ) : (
-        <h1>hi</h1>
-      )
-    }
+        <meta http-equiv = "refresh" content = {"url=" + loginWithRedirect()} />
+      )}
       </div>
-  </>
   );
 };
 
